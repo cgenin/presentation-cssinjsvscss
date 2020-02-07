59944fc44c39e788cfd80628af62b45a05ed116d
C_GENIN <christophe.genin@mutuelledepoitiers.fr>
2020-02-07 14:29:09 +0100

    Modification

---
+++ ex-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+
+    <mon-style/>
+  </div>
+</template>
+
+<script>
+import MonStyle from './components/MonStyle'
+
+export default {
+  name: 'App',
+  components: {
+    MonStyle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ex-vue/src/components/MonStyle.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2 class="mon-titre">Mon test de style</h2>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
+<style scoped>
+    .mon-titre {
+        color: blue;
+        font-weight: bold;
+    }
+</style>
